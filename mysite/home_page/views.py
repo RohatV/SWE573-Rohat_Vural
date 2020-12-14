50767bd2ba363d6e74aa523f096c48b2d4d84ff8
rohat vural <rohat_vural@hotmail.com>
2020-12-14 20:14:26 +0300

    template added and some modification has been made

--- mysite/home_page/views.py
+++ mysite/home_page/views.py
@@ -1,7 +1,15 @@
+from typing import ContextManager
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home_view(request):
-    return HttpResponse("<b>Welcome to my website</b>")
+    
+    if request.user.is_authenticated:
+        username=request.user.username
+        context={"name":username}
+    else:
+        context={"name":"visitor"}
+
+    return render(request,"home_page.html", context)
